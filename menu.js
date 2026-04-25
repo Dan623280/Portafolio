456096df669e2cb37d90414217b030f8793304e4
Nesdael <danieldurfue7@gmail.com>
2026-04-24 20:54:09 -0500

    Terminado

---
+++ menu.js
@@ -0,0 +1,9 @@
+
+
+let img = document.getElementsByClassName("img-b-aside")[0]
+let menu = document.getElementById("aside");
+
+img.addEventListener("click", function(){
+    menu.classList.toggle("activo")
+});
+    
